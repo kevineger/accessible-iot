1e6f01b1cd1a6de7a008ae97f62720621df1d031
Kevin Eger <egerkevinjames@gmail.com>
2019-02-26 14:41:51 -0800

    Fixes consumer groups

--- HelpService/Functions/EventHubTriggerPersistLocation.cs
+++ HelpService/Functions/EventHubTriggerPersistLocation.cs
@@ -7,9 +7,9 @@ using Newtonsoft.Json;
 
 public static class EventHubTriggerPersistLocation
 {
-    // [FunctionName("EventHubTriggerPersistLocation")]
+    [FunctionName("EventHubTriggerPersistLocation")]
     public static async System.Threading.Tasks.Task RunAsync(
-    [EventHubTrigger("stegawiothub", Connection = "EventHubConnStr", ConsumerGroup = "EventHubConsumerGroup")] EventData eventHubMessage,
+    [EventHubTrigger("stegawiothub", Connection = "EventHubConnStr", ConsumerGroup = "azurefunctionsconsumergroup")] EventData eventHubMessage,
     DateTime enqueuedTimeUtc,
     Int64 sequenceNumber,
     string offset,
@@ -28,17 +28,18 @@ public static class EventHubTriggerPersistLocation
 
         // -------------
 
-        // var container = ContainerHelper.Build(context);
-        // var repo = (ICurrentLocationRepository)container.GetService(typeof(ICurrentLocationRepository));
-        // var res = await repo.SaveLocationAsync(userLocation);
+        var container = ContainerHelper.Build(context);
+        var repo = (ICurrentLocationRepository)container.GetService(typeof(ICurrentLocationRepository));
+        var userLocation = JsonConvert.DeserializeObject<UserLocation>(Encoding.UTF8.GetString(eventHubMessage.Body));
+        var res = await repo.SaveLocationAsync(userLocation);
 
-        // if (res)
-        // {
-        //     log.LogInformation($"Successfully stored location. User: {userLocation.UserId}. Lat: {userLocation.Location.gpsLat}. Long: {userLocation.Location.gpsLong}.");
-        // }
-        // else
-        // {
-        //     log.LogError($"Failed to save user location.");
-        // }
+        if (res)
+        {
+            log.LogInformation($"Successfully stored location. User: {userLocation.UserId}. Lat: {userLocation.Location.gpsLat}. Long: {userLocation.Location.gpsLong}.");
+        }
+        else
+        {
+            log.LogError($"Failed to save user location.");
+        }
     }
 }
