abfce81b828b1f868ad2103564d43f41cca947d7
Kevin Eger <egerkevinjames@gmail.com>
2019-02-26 10:27:25 -0800

    Cleanup

--- LocationManager/Functions/HttpTriggerAcknowledge.cs
+++ LocationManager/Functions/HttpTriggerAcknowledge.cs
@@ -26,6 +26,7 @@ namespace FunctionApp
             ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            
             var container = ContainerHelper.Build(context);
             var acknowledge = await RequestHelper.ReadAsync<Acknowledge>(req, log);
             var acknowledgeRepo = (IAcknoledgeRepository)container.GetService(typeof(IAcknoledgeRepository));
@@ -50,10 +51,6 @@ namespace FunctionApp
             {
                 return (ActionResult)new OkObjectResult("Help is already dispatched. Thank you!");
             }
-
-            //return caretaker != null
-            //? (ActionResult)new OkObjectResult($"Hello, you are {caretaker} with GPS location Long {gpslongcaretaker} and Lat {gpslatcaretaker}. You want to {action} for {individual} with GPS location Long {gpslongindividual} and Lat {gpslatindividual}")
-            //: new BadRequestObjectResult("Please pass a caretaker on the query string or in the request body");
         }
 
         private static async Task<string> GetAsync(string url)
@@ -70,24 +67,6 @@ namespace FunctionApp
         private static string GetRouteQueryParam(double startLat, double startLon, double endLat, double endLon)
         {
             return $"&query={startLat},{startLon}:{endLat},{endLon}";
-        }
-
-        public class AckEntity : TableEntity
-        {
-            public AckEntity(string skey, string srow)
-            {
-                this.PartitionKey = skey;
-                this.RowKey = srow;
-            }
-
-            public AckEntity() { }
-            public string Caretaker { get; set; }
-            public string Action { get; set; }
-            public string Individual { get; set; }
-            public double GPSLongCaretaker { get; set; }
-            public double GPSLatCaretaker { get; set; }
-            public double GPSLongIndividual { get; set; }
-            public double GPSLatIndividual { get; set; }
         }
     }
 }
--- LocationManager/Models/API/Acknowledge.cs
+++ LocationManager/Models/API/Acknowledge.cs
@@ -1,9 +1,20 @@
+using Newtonsoft.Json;
+
 public class Acknowledge
 {
+    [JsonProperty("careTakerUserId")]
     public string CareTakerUserId { get; set; }
+
+    [JsonProperty("careRecipientId")]
     public string CareRecipientId { get; set; }
+
+    [JsonProperty("action")]
     public CareTakerAction Action { get; set; }
+
+    [JsonProperty("careTakerLocation")]
     public GPSLocation CareTakerLocation { get; set; }
+
+    [JsonProperty("careRecipientLocation")]
     public GPSLocation CareRecipientLocation { get; set; }
 
     public enum CareTakerAction
